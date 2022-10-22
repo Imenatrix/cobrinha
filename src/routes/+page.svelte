e35f4d35b74068a92cf7379d7f5dbbc4ac3d1a66
Ruan Gomes <ruangomesdorosario@gmail.com>
2022-10-22 11:37:17 -0300

    Create basic board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Block from '$lib/components/block.svelte'
+
+    const WIDTH = 20
+    const HEIGHT = 20
+
+</script>
+
+<div class="container">
+    <div class="board">
+        {#each new Array(HEIGHT) as row}
+            {#each new Array(WIDTH) as block}
+                <div class="block-container">
+                    <Block />
+                </div>
+            {/each}
+            <br>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        widows: 100vw;
+    }
+    .block-container {
+        display: inline-block;
+        margin: 1px;
+    }
+</style>
