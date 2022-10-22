d666c0083118cc18509676a9a2b0a41fe5d76f0d
Ruan Gomes <ruangomesdorosario@gmail.com>
2022-10-22 11:37:09 -0300

    Implement basic block component

---
+++ src/lib/components/block.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let type : 'block' | 'segment' | 'fruit' = 'block'
+</script>
+
+<div class="block"
+    class:segment={type === 'segment'}
+    class:fruit={type === 'fruit'}
+></div>
+
+<style>
+    .block {
+        width: 30px;
+        height: 30px;
+        background-color : gray;
+        display: inline-block;
+        vertical-align: top;
+    }
+    .segment {
+        background-color : green;
+    }
+    .fruit {
+        background-color : red;
+    }
+</style>
